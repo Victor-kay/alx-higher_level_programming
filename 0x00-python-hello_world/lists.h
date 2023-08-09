3304991d4fa9bf822e0b5d723166ffa65adf9beb
Victor-kay <jvictorkingorikay94@gmail.com>
2023-08-09 16:50:58 -0700

    edit lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,48 +1,26 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer value
- * @next: points to the next node
+ * struct listint_s - Node of a singly linked list
+ * @n: integer data in the node
+ * @next: points to the next node in the list
  *
- * Description: Structure for a singly linked list node.
+ * Description: singly linked list node structure
+ * Each node holds an integer value (@n) 
+ * and a reference to the next node (@next).
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-/**
- * print_listint - Prints all elements of a listint_t list.
- * @h: Pointer to the head of the list.
- * Return: Number of nodes in the list.
- */
 size_t print_listint(const listint_t *h);
-
-/**
- * add_nodeint - Adds a new node at the beginning of a listint_t list.
- * @head: Pointer to a pointer to the head of the list.
- * @n: Integer value to store in the new node.
- * Return: Address of the new element, or NULL on failure.
- */
 listint_t *add_nodeint(listint_t **head, const int n);
-
-/**
- * free_listint - Frees a listint_t list.
- * @head: Pointer to the head of the list to be freed.
- */
 void free_listint(listint_t *head);
-
-/**
- * check_cycle - Checks if a loop exists in a listint_t linked list.
- * @list: Pointer to the head of the list to be checked.
- * Return: 0 if no cycle, 1 if a cycle is present.
- */
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
