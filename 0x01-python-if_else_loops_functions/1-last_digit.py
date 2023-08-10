4bafe9e999e27c05ba64c51b7623d814def1976c
Victor-kay <jvictorkingorikay94@gmail.com>
2023-08-10 08:57:54 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,11 @@ print("is", end=" ")
 
 last_digit = abs(number) % 10
 
+print(last_digit, end=" ")
+
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-
