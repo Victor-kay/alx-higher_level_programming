b3093b6728048ec7c5ede034b88a7ca398e84ada
Victor-kay <jvictorkingorikay94@gmail.com>
2023-08-10 09:54:09 -0700

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,11 +2,7 @@
 # 7-islower.py
 
 def islower(c):
-    if ord('a') <= ord(c) <= ord('z'):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
-    return False
-
-# Test cases
-print(islower('a'))  # True
-print(islower('Z'))  # False
-print(islower('7'))  # False
+    else:
+        return False
