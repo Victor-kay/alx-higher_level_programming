35bd8e75747853e2acea4e9ea86d854037b83288
Victor-kay <jvictorkingorikay94@gmail.com>
2023-08-17 17:05:22 -0700

    2. How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 
-import sys
-
 if __name__ == "__main__":
-    argv_len = len(sys.argv) - 1
-    print("{} argument{}{}:".format(
-        argv_len,
-        "s" if argv_len != 1 else "",
-        ":" if argv_len != 0 else "."
-    ))
+    """Handle basic arithmetic operations."""
+    import sys
 
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./your_program_name.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if sys.argv[2] == "+":
+        result = a + b
+    elif sys.argv[2] == "-":
+        result = a - b
+    elif sys.argv[2] == "*":
+        result = a * b
+    elif sys.argv[2] == "/":
+        if b == 0:
+            print("Error: Division by zero.")
+            sys.exit(1)
+        result = a / b
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
