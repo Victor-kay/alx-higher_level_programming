006d6f8f663fdc34af22e517ad85e0d9fd346262
Victor-kay <jvictorkingorikay94@gmail.com>
2023-08-28 04:08:26 -0700

    Write a function that executes a function safely.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,18 +3,21 @@
 import sys
 
 def safe_print_integer_err(value):
-    """Print an integer and handle errors.
+    """Prints an integer with "{:d}".format().
+
+    If a ValueError message is caught, a corresponding
+    message is printed to standard error.
 
     Args:
-        value: The value to be printed.
+        value (int): The integer to print.
 
     Returns:
-        True if the value is an integer and has been correctly printed, False otherwise.
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
     """
     try:
-        value_as_int = int(value)
-        print("{:d}".format(value_as_int))
+        print("{:d}".format(value))
         return True
-    except ValueError:
-        print("Exception: Invalid input. Value is not an integer.", file=sys.stderr)
+    except (TypeError, ValueError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
