b8f16164dcaf5600d6a0b9bf2f61cd2790898a2f
Victor-kay <jvictorkingorikay94@gmail.com>
2023-09-12 06:21:51 -0700

    2. Append to a file mandatory Write a function that appends a string at the end of a text file (UTF8) and returns the number of characters added:

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,9 +6,9 @@ def append_write(filename="", text=""):
     Args:
         filename (str): The name of the file to append to.
         text (str): The string to append to the file.
-    
     Returns:
         int: The number of characters appended.
     """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
+
