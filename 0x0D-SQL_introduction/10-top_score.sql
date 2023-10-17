37bb96cdc150f99d7914050eb361b9f1536be3cf
Victor-kay <jvictorkingorikay94@gmail.com>
2023-10-17 14:00:59 -0700

    last sql commit intro

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
 -- List all records of the second_table with score and name, ordered by score (top first)
-SELECT score, name
-FROM hbtn_0c_0.second_table
-ORDER BY score DESC;
-
+SELECT 'score', 'name'
+FROM 'second_table'
+ORDER BY ' score' DESC;
