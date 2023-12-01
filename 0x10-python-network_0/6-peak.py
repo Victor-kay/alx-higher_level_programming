a0a91509f7a42ccc9e02beda0f9cbe2b1603dd5c
Victor-kay <jvictorkingorikay94@gmail.com>
2023-11-30 18:01:06 -0800

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,22 @@
+#!/usr/bin/python3
+"""
+Find a peak in a list of unsorted integers.
+"""
+
 def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
-    def binary_search_peak(left, right):
-        if left == right:
-            return list_of_integers[left]
+    left, right = 0, len(list_of_integers) - 1
 
+    while left < right:
         mid = (left + right) // 2
 
         if list_of_integers[mid] > list_of_integers[mid + 1]:
-            return binary_search_peak(left, mid)
+            # Peak must be on the left side
+            right = mid
         else:
-            return binary_search_peak(mid + 1, right)
+            # Peak must be on the right side
+            left = mid + 1
 
-    return binary_search_peak(0, len(list_of_integers) - 1)
+    return list_of_integers[left]
