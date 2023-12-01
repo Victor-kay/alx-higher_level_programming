d7e981fbc0f1e46af5f923c424a238f51f538a72
root <Victor-kay>
2023-12-01 10:34:30 -0800

    File: 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script fetches the status from https://alx-intranet.hbtn.io/status using urllib.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", utf8_content)
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,2 @@
+Project Title: 0x11. Python - Network #1
+Date 1/12/2023
