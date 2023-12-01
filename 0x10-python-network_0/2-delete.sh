2eb9c96453c8c1f421fe28f64900341c389bf40d
Victor-kay <jvictorkingorikay94@gmail.com>
2023-11-30 17:07:22 -0800

    File: 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
-# This script sends a DELETE request to the URL passed as the first argument and displays the body of the response
+#!/usr/bin/python3
+"""
+This script sends a DELETE request to the URL passed as the first argument
+and displays the body of the response.
+"""
 
-curl -s -X DELETE "$1"
+import sys
+import requests
+
+def delete_request(url):
+    if not url.startswith('http://') and not url.startswith('https://'):
+        url = 'http://' + url
+
+    response = requests.delete(url)
+    print(response.text)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+    delete_request(url)
