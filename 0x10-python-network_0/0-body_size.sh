ef13e7cfd549ada3f433625a25c4c2bddcafb066
Victor-kay <jvictorkingorikay94@gmail.com>
2023-11-30 16:15:47 -0800

    script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,24 @@
-#!/usr/bin/python3
-"""
-Module: my_module
-Description: This module provides utility functions for handling data.
-"""
+#!/bin/bash
+# Script: get_size.sh
+# Description: Fetches a URL using curl, extracts the size of the response body in bytes.
 
-def my_function(data):
-    """
-    Perform a specific operation on the provided data.
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
 
-    Args:
-        data (str): The input data.
+# Fetch the URL using curl in silent mode and store the response in a variable
+response=$(curl -sI "$1")
 
-    Returns:
-        str: The result of the operation.
-    """
-    # Function implementation here
-    pass
+# Extract the Content-Length header from the response
+content_length=$(echo "$response" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r')
+
+# Check if Content-Length is present in the response
+if [ -z "$content_length" ]; then
+    echo "Error: Content-Length not found in the response headers"
+    exit 1
+fi
+
+# Display the size in bytes
+echo "Size of the response body: ${content_length} bytes"
