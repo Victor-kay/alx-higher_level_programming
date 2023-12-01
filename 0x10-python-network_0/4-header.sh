2f5dd0ba621263ed3a9530f05d426bcf96536a16
Victor-kay <jvictorkingorikay94@gmail.com>
2023-11-30 17:24:24 -0800

    File: 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Set the URL and header variables
-url="$1"
-header="X-School-User-Id: 98"
-
-# Send GET request using curl with the specified header
-curl -H "$header" "$url"
+# Sends a GET request to a specified URL with a custom header using curl.
+curl -s -H "X-School-User-Id: 98" "$1"
