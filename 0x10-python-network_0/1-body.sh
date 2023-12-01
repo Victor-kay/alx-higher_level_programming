7758e67d996c1c4002cff12b2cc8eb63c30efe82
Victor-kay <jvictorkingorikay94@gmail.com>
2023-11-30 17:39:59 -0800

    File: 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the given URL and displays the body of a 200 status code response
-
-curl -s -w "%{http_code}" "$1" | awk 'BEGIN { body=0 } { if (body) print $0 } /^$/ { body=1 }' | tail -n 1
+# Sends a GET request to a specified URL using curl and displays the body of the response for a 200 status code.
+curl -s -w "%{http_code}" "$1" | awk '/200/{getline; print}'
