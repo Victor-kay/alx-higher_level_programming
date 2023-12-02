0a833bb6d0db3936e8b553d030d0317036994d2c
root <Victor-kay>
2023-12-01 17:33:54 -0800

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """
-Sends a POST request to a specified URL with an email parameter,
-and displays the body of the response (decoded in utf-8).
+This script sends a POST request to a specified URL with an email parameter
+and displays the body of the response.
 """
 
-import sys
-import urllib.request
 import urllib.parse
+import urllib.request
+import sys
 
 def post_email(url, email):
     """
-    Sends a POST request to the given URL with the provided email parameter.
-    Displays the body of the response (decoded in utf-8).
-
-    :param url: The URL to send the POST request to.
-    :param email: The email parameter to include in the request.
+    Sends a POST request to the specified URL with the email as a parameter
+    and displays the body of the response.
     """
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data=data, method='POST')
@@ -31,5 +28,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    print(f"Your email is: {email}.")
     post_email(url, email)
